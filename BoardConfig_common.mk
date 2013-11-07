f4779e949ead37ca47a65ae4e9ffb039551fd4c3
Nguyen Thanh Trung <nguyenthanh.trung@nomovok.vn>
2013-11-07 17:11:58 +0700

    Some configuration for a fake Wifi Device to make new Gecko WifiManager work

--- BoardConfig_common.mk
+++ BoardConfig_common.mk
@@ -50,6 +50,11 @@ WITH_DEXPREOPT := false
 
 # Wifi related defines
 WPA_SUPPLICANT_VERSION := VER_0_8_X
+# I don't really understand this APD_DRIVER, this is not suitable for the board, but just use this for now
+BOARD_HOSTAPD_DRIVER := WEXT
+WIFI_DRIVER_FW_PATH_STA := "sta"
+#WIFI_DRIVER_FW_PATH_AP  := "ap"
+#WIFI_DRIVER_FW_PATH_P2P := "p2p"
 
 # enable librecovery
 ENABLE_LIBRECOVERY := true
